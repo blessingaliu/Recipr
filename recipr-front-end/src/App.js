87bf9bc670987bd5bb00531f5581e57e820e04d4
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2022-01-11 15:35:56 +0000

    added axios

--- recipr-front-end/src/App.js
+++ recipr-front-end/src/App.js
@@ -1,8 +1,26 @@
 import ShowNav from "./components/Navbar/navbar";
 import "./App.css";
 import ShowSearchbar from "./components/searchbar/searchbar";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+    const [data, setData] = useState({ hits: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://www.themealdb.com/api/json/v1/1/search.php?f=a'
+,
+      );
+      console.log(result.data)
+
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
     return (
         <div className = "App" >
             <ShowNav />
@@ -10,5 +28,7 @@ function App() {
         </div>
     );
 }
+
+
 
 export default App;
