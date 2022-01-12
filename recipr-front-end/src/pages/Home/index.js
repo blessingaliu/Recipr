1aa18dc926786e5126b15e9522d5c1f5bf380c28
Blessing <blessingaliu@ymail.com>
2022-01-12 10:37:35 +0000

    added a failing test for the mealcard component

--- recipr-front-end/src/pages/Home/index.js
+++ recipr-front-end/src/pages/Home/index.js
@@ -1,23 +1,22 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import MealContainer from '../../components/MealContainer/mealContainer';
-import ShowSearchbar from '../../components/searchbar/searchbar';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import MealContainer from "../../components/Mealcontainer/mealContainer";
+import ShowSearchbar from "../../components/searchbar/searchbar";
 
 function Homepage() {
   const [meals, setMeals] = useState([]);
-  useEffect(()=> {
-      axios
-      .get('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
       .then(({ data }) => setMeals(data.meals))
       .catch((error) => console.log(error));
-    }, []);
+  }, []);
 
   return (
-      <div>
-          <MealContainer meals={meals}/>
-      </div>
-  )
+    <div>
+      <MealContainer meals={meals} />
+    </div>
+  );
 }
 
-
-export default Homepage
+export default Homepage;
