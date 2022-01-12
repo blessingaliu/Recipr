4e0474f1c3ef5e3a612f85e112ccbd747cd17234
Blessing <blessingaliu@ymail.com>
2022-01-12 16:28:08 +0000

    added css styling for the login and signup form

--- recipr-front-end/src/pages/Signup/Signup.js
+++ recipr-front-end/src/pages/Signup/Signup.js
@@ -1,31 +1,34 @@
-import React, { useState } from 'react'
-import {Form, Button} from 'react-bootstrap'
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import "./styles.css";
 
 function Signup() {
-    const [ email, setEmail ] = useState('')
-    const [ password, setPassword ] = useState('')
-    return (
-        <Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="signup">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="success" type="submit">
-    Sign Up
-  </Button>
-</Form>
-    )
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group> */}
+        <Button variant="success" type="submit">
+          Sign Up
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
