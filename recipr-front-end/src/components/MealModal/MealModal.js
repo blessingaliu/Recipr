9c29211c20007a91a11c5de3df2ceb749adacc4f
Blessing <blessingaliu@ymail.com>
2022-01-12 14:40:58 +0000

    added the modal pop card for the recipe title and instructions

---
+++ recipr-front-end/src/components/MealModal/MealModal.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+
+function MealModal({ strMeal, strInstructions }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{strMeal}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{strInstructions}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default MealModal;
--- recipr-front-end/src/components/Mealcard/Mealcard.js
+++ recipr-front-end/src/components/Mealcard/Mealcard.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
-import "./styles.css"
+import "./styles.css";
+import MealModal from "../MealModal/MealModal";
 
-function MealCard({ strMeal, strMealThumb }) {
-    return (
-        <Card className="mealCard" data-testid='card' style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={strMealThumb} />
-            <Card.Body>
-                <Card.Title>{strMeal}</Card.Title>
-                <Button variant="success">See recipe</Button>
-            </Card.Body>
-        </Card>
-    );
+function MealCard({ strMeal, strMealThumb, strInstructions }) {
+  return (
+    <Card className="mealCard" data-testid="card" style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={strMealThumb} />
+      <Card.Body>
+        <Card.Title>{strMeal}</Card.Title>
+        {/* <Button variant="success">See recipe</Button> */}
+        <MealModal strMeal={strMeal} strInstructions={strInstructions} />
+      </Card.Body>
+    </Card>
+  );
 }
 
-export default MealCard
+export default MealCard;
