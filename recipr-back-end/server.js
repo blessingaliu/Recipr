14c2b417359c58cb0149bddbe093e73f0127bc3d
Blessing <blessingaliu@ymail.com>
2022-01-13 16:09:14 +0000

    configured server and database to communicate

--- recipr-back-end/server.js
+++ recipr-back-end/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const userRoutes = require("./userRoutes");
 const app = express();
-require('./db')
+
+require("./db");
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+app.use(userRoutes);
 
 app.listen(5000, () => {
   console.log("Server running at 5000");
--- recipr-back-end/userRoutes.js
+++ recipr-back-end/userRoutes.js
@@ -1,6 +1,14 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const User = require("./userModel");
 
-router.post('/users', async (req, res => {
+router.post("/users", async (req, res) => {
+  console.log(req.body);
+  try {
+    const user = await User.create(req.body);
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
 
-    
-}))
+module.exports = router;
