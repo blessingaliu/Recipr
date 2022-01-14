9c23c794c336fc2438a4c6cc01de1b08644ef636
Blessing <blessingaliu@ymail.com>
2022-01-14 11:56:32 +0000

    attempted to connect the front and backend together

--- recipr-back-end/server.js
+++ recipr-back-end/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const userRoutes = require("./userRoutes");
+const cors = require("cors");
 const app = express();
 // app.use(express.json());
 
 require("./db");
+app.use(cors());
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(userRoutes);
 
 app.listen(5001, () => {
-	console.log("Server running at 5001");
+  console.log("Server running at 5001");
 });
--- recipr-front-end/src/pages/Signup/Signup.js
+++ recipr-front-end/src/pages/Signup/Signup.js
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import "./styles.css";
+import axios from "axios";
 
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  function handleSignup(e) {
+    e.preventDefault();
+    if (!email || !password) {
+      return alert("Please fill out the fields");
+    }
+    axios
+      .post("http://localhost:5001/users", { email, password })
+      .then(({ data }) => console.log(data))
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div className="signup">
-      <Form>
+      <Form onSubmit={handleSignup}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -18,7 +36,13 @@ function Signup() {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
+          />
         </Form.Group>
         {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
