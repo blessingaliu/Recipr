b23b863fbf18c093b527769cd9fd2173b17fd0fb
Blessing <blessingaliu@ymail.com>
2022-01-18 20:12:36 +0000

    fixed the searchbar error and removed the empty test

--- recipr-front-end/src/components/MyMealContainer/MyMealContainer.test.js
+++ recipr-front-end/src/components/MyMealContainer/MyMealContainer.test.js
@@ -1,41 +1,31 @@
-import { render, screen, cleanup } from "@testing-library/react";
-import ShowSearchbar from "../searchbar/searchbar";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { MyContext } from "../../context";
 import MyMealContainer from "./MyMealContainer";
 
 afterEach(() => {
   cleanup();
 });
 
-test("Shows Meal Cards", () => {
-  const meals = [
-    {
-      idMeal: "52768",
-      strMeal: "Apple Frangipan Tart",
-      strMealThumb:
-        "https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg",
-    },
-  ];
-}
+test("renders the Meals container in the program", () => {
+  render(
+    <MyContext.Provider value={{ userrecipes: [] }}>
+      <MyMealContainer />
+    </MyContext.Provider>
+  );
+});
 
-//   user(name, email, password, token, favorites, userrecipe);
+// render(<App />);
+// const linkElement = screen.getByText(/Recipr/i);
+// expect(linkElement).toBeInTheDocument();
 
-//   console.log(user.userrecipe);
-
-//   render(<MyMealContainer meals={user.userrecipe} />);
-
-//   expect(screen.getByText("Apple Frangipan Tart")).toBeInTheDocument();
+// test("Shows Meal Cards", () => {
+//   const meals = [
+//     {
+//       idMeal: "52768",
+//       strMeal: "Apple Frangipan Tart",
+//       strMealThumb:
+//         "https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg",
+//     },
+//   ];
 // });
-
-// const user = {
-//   email: "user2@testing.com",
-//   favorites: ["52893"],
-//   name: "user2",
-//   token:
-//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWU1YTExNGUyYjI3M2ZjYTA0MzUzYjciLCJpYXQiOjE2NDI1MDg4NTMsImV4cCI6MTY0MjU0NDg1M30.ZmyAHVuQD96wfw6Y06ZGC8Aqhsoghjf2vzeUZONWmOY",
-//   userrecipe: [
-//     recipeName: "meat pizza",
-//     recipeIngredients: "tomato",
-//     recipeIngredients: "tomato",
-//     recipeInstructions: "cook",
-//   ],
-// }
