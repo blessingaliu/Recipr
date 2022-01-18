fc06eaf9235ab8b6d178514d5619e01ad2557007
Ben Dowsett <bendowsett@gmail.com>
2022-01-18 12:13:17 +0000

    styling for the user recipe modal image

--- recipr-front-end/src/components/MyMealModal/MyMealModal.js
+++ recipr-front-end/src/components/MyMealModal/MyMealModal.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
+import './styles.css'
 
-function MyMealModal({ strMeal, strInstructions, strIngredient1 }) {
+function MyMealModal({ strImage, strMeal, strInstructions, strIngredient1 }) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
 
   return (
     <>
@@ -18,11 +20,21 @@ function MyMealModal({ strMeal, strInstructions, strIngredient1 }) {
           <Modal.Title>{strMeal}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div>
-            
-           {strIngredient1} 
+          <div >
+          <img src={strImage} className='modal-image'/>
+
           </div>
-          {strInstructions}
+
+          <div>
+          <h4>Ingredients</h4>
+          {strIngredient1.split(',').map((ingredient) => (
+          <li>{ingredient}</li>
+            ))}
+          </div><br/>
+          <h4>Instructions</h4>
+          {strInstructions.split(',').map((instruction) => (
+          <li>{instruction}</li>
+            ))}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="success" onClick={handleClose}>
