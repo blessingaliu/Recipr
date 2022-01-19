fa5dc72ab247b2d535f3341a8ac1d21e05c4e05d
aysel bulut <ayselbulut1@gmail.com>
2022-01-19 15:36:10 +0000

    added pics

--- recipr-front-end/src/App.js
+++ recipr-front-end/src/App.js
@@ -13,6 +13,8 @@ import Favorites from "./pages/Favorites/Favorites";
 import MyRecipes from "./pages/Myrecipes/Myrecipes";
 import AddRecipes from "./pages/AddRecipes/addrecipes";
 import Footer from "./components/footer/footer";
+import ImageSlider from './components/slide/ImageSlider';
+import { SliderData } from './components/slide/SliderData';
 
 function App() {
   const { user, setUser } = useContext(MyContext);
@@ -24,6 +26,7 @@ function App() {
       <ShowNav />
       <Switch>
         <Route exact path="/">
+        <ImageSlider slides={SliderData} />
           <ShowSearchbar />
           <Homepage />
         </Route>
