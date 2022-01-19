c65456f25060d300c45e76ec4b9978f9bb479a63
Blessing <blessingaliu@ymail.com>
2022-01-19 14:37:51 +0000

    added styling for the searchbar

--- recipr-front-end/src/pages/AddRecipes/addrecipes.js
+++ recipr-front-end/src/pages/AddRecipes/addrecipes.js
@@ -1,46 +1,47 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import { MyContext } from '../../context';
+import { MyContext } from "../../context";
 import "./styles.css";
 import axios from "../../Axios";
-import { useHistory } from 'react-router-dom'
-import FileBase64 from 'react-file-base64'
+import { useHistory } from "react-router-dom";
+import FileBase64 from "react-file-base64";
 
 function AddRecipes() {
-  const {user} = useContext(MyContext)
+  const { user } = useContext(MyContext);
   const history = useHistory();
   // const {saveRecipe} = useContext(MyContext)
   const [recipeName, setRecipeName] = useState("");
   const [recipeIngredients, setRecipeIngredients] = useState([]);
   const [recipeInstructions, setRecipeInstructions] = useState([]);
-  const [recipeImage, setImage] = useState()
-  console.log(user)
- 
+  const [recipeImage, setImage] = useState();
+  console.log(user);
 
-  function addRecipe (c) {
+  function addRecipe(c) {
     c.preventDefault();
-    
+
     // if (!email || !password || !name) {
     //   return alert("Please fill out the fields");
     // }
     axios
-      .post("/add_recipe", { recipeName, recipeIngredients, recipeInstructions, recipeImage})
-      .then(({ data }) => {
+      .post("/add_recipe", {
+        recipeName,
+        recipeIngredients,
+        recipeInstructions,
+        recipeImage,
       })
+      .then(({ data }) => {})
       .catch((err) => console.log(err));
-      //history.replace('/my_recipes');
-      history.push('/');
-      history.replace('/my_recipes');
-      window.location.reload(false);
+    //history.replace('/my_recipes');
+    history.push("/");
+    history.replace("/my_recipes");
+    window.location.reload(false);
   }
-    return (
-        <div className="signup">
-          
-          
-      <Form onSubmit= { addRecipe }>
-      <h1> Add your recipes here, {user.name}</h1>
+  return (
+    <div className="signup">
+      <Form onSubmit={addRecipe}>
+        <h1> Add your recipes here, {user.name}</h1>
         <Form.Group className="mb-3 form-inline" controlId="formBasicName">
-          <Form.Label>Recipe Name </Form.Label>
+          <Form.Label className="label">Recipe Name </Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter recipe name"
@@ -48,11 +49,10 @@ function AddRecipes() {
             value={recipeName}
             required
           />
-        
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Label>Ingredients</Form.Label>
+          <Form.Label className="label">Ingredients</Form.Label>
           <Form.Control
             type="textarea"
             placeholder="Enter recipe name"
@@ -60,10 +60,9 @@ function AddRecipes() {
             value={recipeIngredients}
             required
           />
-        
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Label>Instructions</Form.Label>
+          <Form.Label className="label">Instructions</Form.Label>
           <Form.Control
             type="textarea"
             placeholder="Enter recipe name"
@@ -71,22 +70,19 @@ function AddRecipes() {
             value={recipeInstructions}
             required
           />
-        
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Label>Recipe Image</Form.Label>
+          <Form.Label className="label">Recipe Image</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter recipe name"
             onChange={(e) => setImage(e.target.value)}
             value={recipeImage}
           />
-         <FileBase64
-          multiple={ false }
-          onDone={ ({base64})=>setImage(
-            base64
-          ) } />
-        
+          <FileBase64
+            multiple={false}
+            onDone={({ base64 }) => setImage(base64)}
+          />
         </Form.Group>
 
         <Button variant="success" type="submit">
@@ -94,7 +90,7 @@ function AddRecipes() {
         </Button>
       </Form>
     </div>
-    )
+  );
 }
 
-export default AddRecipes
+export default AddRecipes;
