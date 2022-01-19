bcf875fc816d7d487e9e6f270d5e5363c03bca40
Blessing <blessingaliu@ymail.com>
2022-01-19 16:20:18 +0000

    added a footer to the home page

--- recipr-front-end/src/App.js
+++ recipr-front-end/src/App.js
@@ -12,6 +12,7 @@ import axios from "./Axios";
 import Favorites from "./pages/Favorites/Favorites";
 import MyRecipes from "./pages/MyRecipes/myrecipes";
 import AddRecipes from "./pages/AddRecipes/addrecipes";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const { user, setUser } = useContext(MyContext);
@@ -25,6 +26,7 @@ function App() {
         <Route exact path="/">
           <ShowSearchbar />
           <Homepage />
+          <Footer />
         </Route>
         {!user && (
           <>
@@ -52,6 +54,7 @@ function App() {
           </Route>
         )}
       </Switch>
+      {/* <Footer /> */}
     </Router>
   );
 }
