66b21956f8b3801e054a888178a8cb9a1db3ce1d
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2022-01-20 11:17:32 +0000

    Styling title

--- recipr-front-end/src/components/Navbar/navbar.js
+++ recipr-front-end/src/components/Navbar/navbar.js
@@ -19,33 +19,21 @@ function ShowNav() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        {/* <LinkContainer to="/">
-          <Navbar.Brand>Recipr</Navbar.Brand>
-        </LinkContainer> */}
-        <Nav.Link href="/">Recipr</Nav.Link>
+        <Nav.Link href="/" style={{fontWeight: "900"}}>Recipr</Nav.Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav.Link href="/">Home</Nav.Link>
           {!user && (
             <Nav className="me-auto">
-              {/* <LinkContainer to="/login"> */}
               <Nav.Link href="/login">Login</Nav.Link>
-              {/* </LinkContainer> */}
-              {/* <LinkContainer to="/signup"> */}
               <Nav.Link href="/signup">Signup</Nav.Link>
-              {/* </LinkContainer> */}
             </Nav>
           )}
           {user && (
             <>
-              {/* <LinkContainer to="/my-favorites"> */}
               <Nav.Link href="/my-favorites">Favorites</Nav.Link>
-              {/* <LinkContainer to="/add_recipe"> */}
               <Nav.Link href="/add_recipe">Add Recipe</Nav.Link>
-              {/* </LinkContainer> */}
-              {/* <LinkContainer to="/my_recipes"> */}
               <Nav.Link href="/my_recipes">My Recipes</Nav.Link>
-              {/* </LinkContainer> */}
               <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
             </>
           )}
